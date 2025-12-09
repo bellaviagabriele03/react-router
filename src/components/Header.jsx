21e7426b07a07c8b1ee194fc1df56884de95a9a8
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-09 18:49:07 +0100

    feat: logo, link della navbar aggiunti tramite map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,43 @@
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+
+    const Links = [
+        {
+            title: "Home",
+            path: "/",
+        },
+        {
+            title: "Chi Siamo",
+            path: "/about",
+        },
+        {
+            title: "I nostri Prodotti",
+            path: "/products",
+        },
+        
+    ]
+
+
+
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <header className="">
+            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Boolando</Link>
+                    <Link className="navbar-brand" to="/">
+                        <img src="" alt="" />
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
-                            <NavLink className="nav-link" to="/about">About</NavLink>
-                            <NavLink className="nav-link" to="/products">Pricing</NavLink>
-
+                           {Links.map((link, index) => {
+                            return (
+                                <NavLink className="nav-link" aria-current="page" to={link.path}>{link.title}</NavLink>
+                            )
+                           })}
+                            
                         </div>
                     </div>
                 </div>
