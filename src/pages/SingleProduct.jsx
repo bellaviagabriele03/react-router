2017b320dec21a2f78110d7e7d7d7ec55b16bbf5
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-10 19:10:52 +0100

    feat: aggiunta pagina singoli prodotti

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,15 +1,43 @@
+import axios from "axios"
 import { useParams, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 
 
 export default function SingleProduct() {
 
     const { id } = useParams()
-   
-    
+    const [single, setSingle] = useState()
+
+    function getSingleProduct() {
+        axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+
+            setSingle(resp.data)
+
+        })
+            
+       
+    }
+
+
+    useEffect(() => {
+        getSingleProduct();
+    }, [id])
+
+
     return (
         <div className="container">
-            <h1>Prodotto n*{id}</h1>
+            <div className="row">
+                <div className="col-12">
+                   <div className="card h-100 ">
+                    <img className="card-img" src={single.image} alt="" />
+                    <div className="card-body">
+                        <h3 className="text-center">{single.title}</h3>
+                    </div>
+                   </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
